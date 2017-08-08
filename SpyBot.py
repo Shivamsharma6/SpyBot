ff456227355a0c60c8bf6f4c6f13b98b7392d6bf
Shivamsharma6 <Shivamsharma6@gmail.com>
2017-08-08 15:57:04 +0530

    8th Aug Changes

--- SpyBot.py
+++ SpyBot.py
@@ -25,34 +25,64 @@ print ''
 print 'Featuring the most advanced A.I spy of our time!'
 print 'Heyy'
 gender=raw_input('Male or Female:')
-if(gender=='Male'or'm'or'male'or'MALE'or'M'):
+if ((gender.upper()=='MALE') or (gender.upper()=='M')):
     name=raw_input("Enter your name:")
     print ''
-    time.sleep(0.2)
+    time.sleep(0.5)
     print 'I am a Spy'
     print 'Just for folks!!!'
     print ''
     print 'Welcome'
-    print 'Mr./Ms.'+name
-    age=int(raw_input('Enter you age:'))
+    marital=raw_input('Married or not? \n')
+    if marital=='Yes':
+        salutation='Mr.'
+        print salutation+name
+    else:
+        salutation='Mr.'
+        print salutation+name
+    print 'Alright,I would liketo know a little more about you...'
+    age=raw_input('Enter you age:')
+
+    while(age.isalpha()):
+        print 'Enter a numeric value'
+        age=int(raw_input('Enter your age:'))
+
+
     year = str(datetime.date.today().year-age)
     print 'So lemme guess...'
     time.sleep(1)
     print 'You were born in:',year
+    if age<100:
+        old=100+int(year)
+        print 'You will be 100 years old in:',old
+    else:
+        old=100+int(year)
+        print 'You were 100 years older in:',old
 
-
-elif(gender=='Female'or'F'or'FEMALE'or'f'):
+elif ((gender.upper()=='FEMALE') or (gender.upper()=='F')):
     name=raw_input("Enter your name:")
     print ''
-    time.sleep(0.2)
+    time.sleep(0.5)
     print 'I am a Spy'
     print 'Just for folks!!!'
     print ''
     print 'Welcome'
-    print 'Mrs./Miss.'+name
+    marital=raw_input('Married or not ? \n')
+    if ((marital.upper()=='YES') or (marital.upper()=='MARRIED')or(marital.upper()=='NOT')):
+        salutation='Mrs.'
+        print salutation+name
+    else:
+        salutation='Ms.'
+        print salutation + name
     age=int(raw_input('Enter you age:'))
     year = str(datetime.date.today().year-age)
     print 'So lemme guess...'
     time.sleep(1)
-    print 'You were born in:',year
-
+    if age<100:
+        old=100+int(year)
+        print 'You will be 100 years old in:',old
+    else:
+        old=100+int(year)
+        print 'You were 100 years older in:',old
+else:
+    print 'I do not think you have gender.'
